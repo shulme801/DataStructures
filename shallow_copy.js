c4481427ea6a7c7d3da88bbf503e9b94282894d4
shulme801 <shulme801@gmail.com>
2018-03-30 16:41:20 -0400

    First Commit of Chapter 2 stuff

---
+++ shallow_copy.js
@@ -0,0 +1,18 @@
+var test = [[1,2,3],[4,5,8],10];
+var test2 = [];
+
+/* console.log("Values of test2 in loop");
+for (var i=0; i < test.length; i++) {
+
+	test2[i] = test[i];
+	console.log("test2 [",i,"]=",test2[i]);
+}
+
+*/
+
+test2 = test; // Shallow Copy
+test2[1][2] = 6;
+test[2] = 20;
+
+console.log("Here is test2 ",test2);
+console.log("Here is test ",test);
